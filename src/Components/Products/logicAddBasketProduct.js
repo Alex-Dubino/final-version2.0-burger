8e510a90d86a5f6d08033218f8cc8e4475073576
Alexey Dubino <alexd2008add@gmail.com>
2024-02-14 18:11:44 +0300

    second commit

---
+++ src/Components/Products/logicAddBasketProduct.js
@@ -0,0 +1,27 @@
+export default function logicAddBasketProduct(item, stateBasket) {
+  console.log(stateBasket)
+  const addCopyProductItemValidate = stateBasket.basket.find(
+    (elem) => elem.title === item.title
+  );
+
+  if (addCopyProductItemValidate) {
+    const updateBasket = stateBasket.basket.map((element) => {
+      if (element.title === addCopyProductItemValidate.title) {
+        element.count += 1;
+        return element;
+      }
+      return element;
+    });
+
+    stateBasket.setBasket(updateBasket);
+    return;
+  }
+
+  const copyItem = { ...item };
+  const lastElementIdBasket =
+    stateBasket.basket.length > 0
+      ? stateBasket.basket[stateBasket.basket.length - 1].id
+      : 0;
+  copyItem.id = +lastElementIdBasket.id;
+  stateBasket.setBasket((prevState) => [...prevState, copyItem]);
+}
